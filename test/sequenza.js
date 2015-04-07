5d93916310f295692a472004287eaa1c409ff99c
Gibran Malheiros <gibatronic@gmail.com>
2015-04-07 00:44:48 -0300

    implemented basic prototype structure

--- test/sequenza.js
+++ test/sequenza.js
@@ -1,8 +1,23 @@
 var Sequenza = require('../lib/sequenza');
 
 describe('sequenza', function() {
+  var hasMethod = function(name) {
+    return name in Sequenza.prototype && Sequenza.prototype[name] instanceof Function;
+  };
+
+  var isFunction = function(object) {
+    return object instanceof Function;
+  };
+
   it('should be a function', function() {
-    var isFunction = Sequenza instanceof Function;
-    expect(isFunction).toBe(true);
+    expect(isFunction(Sequenza)).toBeTruthy();
+  });
+
+  it('should have the "queue" method', function() {
+    expect(hasMethod('queue')).toBeTruthy();
+  });
+
+  it('should have the "start" method', function() {
+    expect(hasMethod('start')).toBeTruthy();
   });
 });
